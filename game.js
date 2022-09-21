dfcb4b9baef722f4c0927bd846da98e73a4041c8
john <john.davis.2015.03@gmail.com>
2022-09-20 22:41:49 -0500

    array bidimensional

--- game.js
+++ game.js
@@ -1,43 +1,47 @@
+// window.innerWidth
+// window.innerHeight
+
+// game.fillRect(0, 0, 100, 100); // negro
+// game.clearRect(50, 50, 50, 50); // negro
+// game.clearRect();
+// game.clearRect(0,0,50,50);
+
+// game.font = '25px Verdana';
+// game.fillStyle = 'purple';
+// game.textAlign = 'center';
+// game.fillText('platzi', 25, 25)
+
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementsSize;
 
-function startGame() {
-  let canvasSize;
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
+function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
     canvasSize = window.innerWidth * 0.8;
   } else {
     canvasSize = window.innerHeight * 0.8;
   }
 
-  canvasSize = Math.floor(canvasSize);
-
+  // canvasSize = Math.floor(canvasSize);
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
+  elementsSize = canvasSize / 10;
 
-  const elementsSize = canvasSize / 10;
+  startGame();
+}
 
+function startGame() {
   console.log({ canvasSize, elementsSize });
 
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
 
   for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis['X'], elementsSize * i, elementsSize);
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
   }
-
-  // window.innerWidth
-  // window.innerHeight
-
-  // game.fillRect(0, 0, 100, 100); // negro
-  // game.clearRect(50, 50, 50, 50); // negro
-  // game.clearRect();
-  // game.clearRect(0,0,50,50);
-
-  // game.font = '25px Verdana';
-  // game.fillStyle = 'purple';
-  // game.textAlign = 'center';
-  // game.fillText('platzi', 25, 25)
 }
